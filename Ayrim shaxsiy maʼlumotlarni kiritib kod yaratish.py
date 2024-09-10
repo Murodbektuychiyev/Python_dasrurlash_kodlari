2dff01d7f42553834a2f0d3c1575766b7d7c868d
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-10 20:09:04 +0500

    Add files via upload
    Ayrim shaxsiy maʼlumotlarni kiritib kod yaratish

---
+++ Ayrim shaxsiy maʼlumotlarni kiritib kod yaratish.py
@@ -0,0 +1,11 @@
+ism = 'Murodbek'  #Ismim
+familiya = 'Tuychiyev'   #Familiyam
+yosh = '15'   #Yoshim
+viloyat = 'kashkadaryo'   #Viloyatim
+tuman = 'koson'    #Tumanim
+print(ism)
+print(familiya)
+print(yosh)
+print(viloyat)
+print(tuman)
+print("Hamma kodlar bajarildi ✅👍")
