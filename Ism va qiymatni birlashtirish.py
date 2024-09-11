f3bac736b8e61571657f5164200f74214e70c6c8
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-12 00:09:07 +0500

    Add files via upload
    Ism va qiymatni birlashtirish

---
+++ Ism va qiymatni birlashtirish.py
@@ -0,0 +1,9 @@
+#Created by Murodbek Tuychiyev
+#Ism va qiymatni birlashtirish
+
+ism = 'Murodbek'
+familiya = 'Tuychiyev'
+yosh = 15
+yosh = str(yosh)
+xabar = ism+' ' +familiya+' ' +yosh+ ' yoshda'
+print(xabar)
