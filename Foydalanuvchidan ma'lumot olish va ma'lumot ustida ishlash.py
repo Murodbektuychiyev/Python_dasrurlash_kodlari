8879f834f2898a163b58c7b04609ca2f052437b9
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-11 21:40:40 +0500

    Add files via upload
    Foydalanuvchidan ma'lumot olish va ma'lumot ustida ishlash

---
+++ Foydalanuvchidan ma'lumot olish va ma'lumot ustida ishlash.py
@@ -0,0 +1,4 @@
+#Foydalanuvchidan ma'lumot olish va ma'lumot ustida ishlash
+ism = input("Ismingizni nima?\n>>>")
+print("O\'zbek tili: Assalomu Alaykum, Tizimga xush kelibsiz                                      In English: Hello, Welcome to the system!    Русский язык:Здравствуйте, добро пожаловать в систему!",ism.title())
+#Ismingizni kiritasiz va "Assalomu alaykum... " degan javobni olasiz        
