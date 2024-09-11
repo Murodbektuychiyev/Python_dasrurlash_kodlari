7e3447c7b58bde4e5192bb7411b616e5c1d16a11
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-11 06:41:01 +0500

    Add files via upload
    Sonlarni koʻpaytirish

---
+++ Sonlarni koʻpaytirish.py
@@ -0,0 +1,6 @@
+#Sonlarni ko'paytirish:
+x=3
+y=4
+print(x*y) #Javob 12-chiqadi
+# Yulduzcha* qo'yish qoidalari. * bi dona qo'ysak ko'paytirish; Agar ikkita qo'ysak darajaga oshirish bo'ladi
+#Agar *** uchta qo'ysak, "SyntaxError" deb javob beradi yani xatolik
