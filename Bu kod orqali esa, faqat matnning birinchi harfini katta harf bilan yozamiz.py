039266cbd6631e8210cf77b95dbee8bc1babf5ce
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-11 17:37:36 +0500

    Add files via upload
    Bu kod orqali esa, faqat matnning birinchi harfini katta harf bilan yozamiz

---
+++ Bu kod orqali esa, faqat matnning birinchi harfini katta harf bilan yozamiz.py
@@ -0,0 +1,5 @@
+#Bu kod orqali esa, faqat matnning birinchi harfini katta harf bilan yozamiz
+carname = '🚘 damas'
+additionalfeature = 'deluxe'
+print(carname.capitalize() + ' ' + additionalfeature)
+#Kodimizni ishga tushursak: Damas deluxe deb chiqadi ✅
