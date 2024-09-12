6accc32a33ea6782e511ee32f04612fffdfd3417
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-12 22:08:44 +0500

    Delete Ro'yhatimizdan faqat bitta narsani chiqarib olishimiz uchun kerak boʻladian kod_.py

--- Ro'yhatimizdan faqat bitta narsani chiqarib olishimiz uchun kerak boʻladian kod_.py
+++
@@ -1,6 +0,0 @@
-#Created by Murodbek Tuychiyev
-
-#Ro'yhatimizdan faqat bitta narsani chiqarib olishimiz uchun kerak boʻladian kod:
-mevalar = ['olma', 'anjir', 'nok', "o'rik"]
-print(mevalar[0])
-#Natija: olma
