1e5db5790f81a822259c6b2ac7e0d91b86fa0c7b
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-26 22:02:19 +0500

    Delete Ikkita o'zgaruvchini bir-biriga qoʻshish.py
    Del

--- Ikkita o'zgaruvchini bir-biriga qoʻshish.py
+++
@@ -1,9 +0,0 @@
-#Ikkita o'zgaruvchini bir-biriga qoʻshish
-ism = 'Murodbek'
-print("Mening ismim", ' '+ism)
-
-
-ism = 'Murodbek'
-familiya = 'Tuychiyev'
-print(ism +' ' +familiya)
-# ' ' bu narsa joy tashlash uchun ishlatilmoqda
