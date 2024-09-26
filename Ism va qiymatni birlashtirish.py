7e4a753c53305def31c150aa8d1793b0f927a2de
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-26 22:01:39 +0500

    Delete Ism va qiymatni birlashtirish.py
    Delete

--- Ism va qiymatni birlashtirish.py
+++
@@ -1,9 +0,0 @@
-#Created by Murodbek Tuychiyev
-#Ism va qiymatni birlashtirish
-
-ism = 'Murodbek'
-familiya = 'Tuychiyev'
-yosh = 15
-yosh = str(yosh)
-xabar = ism+' ' +familiya+' ' +yosh+ ' yoshda'
-print(xabar)
