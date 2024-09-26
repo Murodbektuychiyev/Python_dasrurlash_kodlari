4aa4936a5c10cbb1fcdc23f6030748436befa60f
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2024-09-26 21:47:55 +0500

    Delete Yoshni hisoblaydigan dastur.py
    Del

--- Yoshni hisoblaydigan dastur.py
+++
@@ -1,5 +0,0 @@
-#Yoshni hisoblaydigan dastur
-yil = int(input("To\'g\'ilgan yilingizni kiriting: \n>>>"))
-yosh = 2025-yil
-print("Siz",yosh,"yoshda ekansiz")
-#Eslatma: Yoshni 2025-yildan sizni to'g'ilgan yilingizni ayirib hisoblab beradi
